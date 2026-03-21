b4622973c7ac3e22c199e53bdfff5dbf737c2a4b
tounST <tounzx2@gmail.com>
2026-03-22 01:26:47 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Version: v1.0 — 2026-03-20
 // ═══════════════════════════════════════════════════
 
-const CACHE_NAME = 'po-tracker-v1';
+const CACHE_NAME = 'po-tracker-v2';
 
 // ไฟล์ที่จะ cache ไว้ใช้ offline
 const CACHE_FILES = [
