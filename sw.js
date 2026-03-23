ee38a1aac1356eec88878f8bc72d430a11755cf0
tounST <tounzx2@gmail.com>
2026-03-23 17:28:32 +0700

    Update CACHE_NAME to version v5

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Version: v1.0 — 2026-03-20
 // ═══════════════════════════════════════════════════
 
-const CACHE_NAME = 'po-tracker-v4';
+const CACHE_NAME = 'po-tracker-v5';
 
 // ไฟล์ที่จะ cache ไว้ใช้ offline
 const CACHE_FILES = [
